292467c84253756362a6d39f12b7ba49a7e4384c
natalete <natsalete14@gmail.com>
2024-01-08 13:54:58 -0300

    Bondinho

---
+++ Bondinho.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   int a, m;
+
+   cin >> a >> m;
+
+   if (a + m <= 50){
+      cout << "S" << endl;
+   }else {
+      cout << "N" << endl;
+   }
+
+
+    return 0;
+}
