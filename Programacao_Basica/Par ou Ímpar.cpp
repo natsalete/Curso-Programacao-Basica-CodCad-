0b1c0939c0c4ab8acb047604b0e687e3a91f555f
natalete <natsalete14@gmail.com>
2024-01-08 14:30:58 -0300

    Par ou Ímpar

---
+++ Programacao_Basica/Par ou Ímpar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int b, c;
+
+   cin >> b >> c;
+
+   if((b + c) % 2 == 0){
+     cout << "Bino" << endl;
+
+   } else {
+     cout << "Cino" << endl;
+   }
+
+
+    return 0;
+}
